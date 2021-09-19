fa058ba87b81294c5bd3de0a0218c3ec15eee128
Amandine Arias <amandine.arias@gmail.com>
2021-09-19 18:10:33 +0200

    feat (data) : create & update

---
+++ kasa/src/Data/AProposData.js
@@ -0,0 +1,19 @@
+export const AProposData = [
+  {
+    title: 'Fiabilité',
+    content: 'Lorem ipsum',
+  },
+  {
+    title: 'Respect',
+    content:
+      'La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.',
+  },
+  {
+    title: 'Service',
+    content: 'Lorem ipsum',
+  },
+  {
+    title: 'Responsabilité',
+    content: 'Lorem ipsum',
+  },
+];
