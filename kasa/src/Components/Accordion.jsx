b63365922a685e255f41f9f2c1214dba4d40f8b9
Amandine Arias <amandine.arias@gmail.com>
2021-09-21 20:39:29 +0200

    fix (accordion) : key

--- kasa/src/Components/Accordion.jsx
+++ kasa/src/Components/Accordion.jsx
@@ -1,20 +1,21 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
 // HELP FORCOMPONENT ERROR
 // https://reactjs.org/docs/lists-and-keys.html#keys
 
 // try to fix error => ADDED : key=id
-const Accordion = (props) => {
+
+const Accordion = ({ id, title, content }) => {
   const [isActive, setIsActive] = useState(false);
-  console.log(props.id);
 
   return (
-    <article key={props.id} className="accordion">
+    <article className="accordion">
       <button
         className="accordion-title"
         onClick={() => setIsActive(!isActive)}
       >
-        <span>{props.title}</span>
+        <span>{title}</span>
         <span>
           {isActive ? (
             <i className="fas fa-chevron-up"></i>
@@ -23,28 +24,23 @@ const Accordion = (props) => {
           )}
         </span>
       </button>
-      {isActive && <p className="accordion-content">{props.content}</p>}
+      {isActive && (
+        <p key={id} className="accordion-content">
+          {content}
+        </p>
+      )}
+      {/* {isActive && <p className="accordion-content">{content}</p>} */}
     </article>
   );
 };
 
+Accordion.propTypes = {
+  title: PropTypes.string.isRequired,
+  content: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.arrayOf(PropTypes.string),
+  ]).isRequired
+  // content: PropTypes.oneOfType([PropTypes.array, PropTypes.string]).isRequired
+};
+
 export default Accordion;
-
-// import React, { useState } from 'react';
-
-// const Accordion = ({ id, title, content }) => {
-//   const [isActive, setIsActive] = useState(false);
-
-//   return (
-//     <article key={id} className="accordion">
-//       <button className="accordion-title" onClick={() => setIsActive(!isActive)}>
-//         <span>{title}</span>
-//         <span>{isActive ? <i className="fas fa-chevron-up"></i> : <i className="fas fa-chevron-down"></i>}</span>
-//       </button>
-//       {isActive && <p key={index} className="accordion-content">{content}</p>}
-//       {/* {isActive && <p className="accordion-content">{content}</p>} */}
-//     </article>
-//   );
-// };
-
-// export default Accordion;
