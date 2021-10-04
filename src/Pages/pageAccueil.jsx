a6d65053755e0b38b05501758ce1045307e35d5d
Amandine Arias <amandine.arias@gmail.com>
2021-10-04 20:49:06 +0200

    remove (footer) : double !

--- src/Pages/pageAccueil.jsx
+++ src/Pages/pageAccueil.jsx
@@ -2,7 +2,6 @@ import React, { Fragment } from 'react';
 import Background from '../Images/accueilBanner.jpg'
 import Banner from '../Components/Banner';
 import Rental from '../Components/Annonces';
-import Footer from '../Components/Footer';
 
 /**
  * Render "Accueil" page
@@ -16,7 +15,6 @@ export default class Accueil extends React.Component {
       <Fragment>
         <Banner src={Background} alt="Paysage" title="Chez vous, partout et ailleurs" />
         <Rental />
-        <Footer />
       </Fragment>
     );
   }
